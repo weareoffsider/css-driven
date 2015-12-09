c214431f0ea047289bd7d4180283521e1f6ff80d
Jonny Scholes <jonnyscholes@gmail.com>
2015-12-09 11:38:25 +1100

    Revert Return early from getLongestTransitionOrAnimationTime for browsers that don't support css transitions. Default to no delay.

--- src/utils.js
+++ src/utils.js
@@ -28,9 +28,6 @@ var getLongestTransitionOrAnimationTime = function( el ){
     var delay, duration, subTotals;
 
     delay = getComputedStyle( el )[compat.prefixed(cssType + "Delay")]
-    if (!delay) {
-      return [0];
-    }
     delay = delay.split(',').map(parseFloat);
     duration = getComputedStyle( el )[compat.prefixed(cssType + "Duration")]
     duration = duration.split(',').map(parseFloat);
